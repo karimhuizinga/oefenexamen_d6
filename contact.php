507fef572d419a33b094f02337df0273b5f67860
karimhuizinga <65898017+karimhuizinga@users.noreply.github.com>
2021-12-15 15:02:21 +0100

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>contact</title>
+    <link rel="stylesheet" href="CSS/contact.css">
+    <link rel="stylesheet" href="CSS/nav-footer.css">
+  </head>
+  <body>
+    <!--navigationbar-->
+    <div id="navigatie">
+      <div class="navigatie-objects">
+        <div class="navigatie-link">
+          /<a href="http://localhost/website/reserveringen.php">reserveringen</a>
+        </div>
+        <div class="navigatie-link">
+          /<a href="http://localhost/website/contact.php">contact</a>
+        </div>
+        <div class="navigatie-link">
+          /<a href="http://localhost/website/cars.php">cars</a>
+        </div>
+        <div class="navigatie-link">
+          <a href="http://localhost/website/">home</a>
+        </div>
+      </div>
+    </div>
+      <br>
+      <div id="main-page">
+        <div class="contact-data">
+          <p>
+            FastDevelopmend<br><br>
+            bedrijfgegevens<br><br>
+            KvK-nummer: 34093684<br>
+            btw-nummer: NL123456789B01<br>
+            straat: molenstraat<br>
+            huisnummer: 7<br>
+            postcode: 9408GP<br>
+            land: Nederland<br>
+            FastDevelopment@gmail.com
+          </p>
+          </div>
+          <br>
+        </div>
+        <div class="footer">
+          <p class="footer-text">© Copyright 2021 FastDevelopment bv</p>
+        </div>
+      </body>
+</html>
